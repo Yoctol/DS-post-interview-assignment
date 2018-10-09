ac365c7023e9d0d886300670f1395f30c0a8bb26
noobOriented <jsaon92@gmail.com>
2018-10-09 18:00:25 +0800

    change Error type.

--- encoder/task.py
+++ encoder/task.py
@@ -2,6 +2,8 @@ import abc
 
 import numpy as np
 
+from .types import SupervisedData
+
 
 class Task(abc.ABC):
     def __init__(self, name, output_dim):
@@ -9,53 +11,52 @@ class Task(abc.ABC):
         self.output_dim = output_dim
 
     @abc.abstractmethod
-    def build_graph(self, graph):
+    def build_graph(self):
         pass
-
-    def __hash__(self):
-        return self.name.__hash__()
 
     @abc.abstractmethod
     def validate_data(self, data):
         pass
 
+    def __hash__(self):
+        return self.name.__hash__()
+
+    def __str__(self):
+        return f"{self.name}"
+
 
 class SupervisedTask(Task):
-    def validate_data(self, data):
-        if len(data) != 2:
-            raise RuntimeError("Data should be (x, y) pairs!")
+    def validate_data(self, data: SupervisedData):
         x, y = data
         if len(x.shape) != 2:
-            raise RuntimeError("Input data should be rank 2!")
+            raise ValueError("Input data should be rank 2!")
         if x.shape[0] != y.shape[0]:
-            raise RuntimeError("Data pairs should have same length!")
+            raise ValueError("Data pairs should have same length!")
         if len(y.shape) != 2:
-            raise RuntimeError("Output data should be rank 2!")
+            raise ValueError("Output data should be rank 2!")
         if y.shape[1] != self.output_dim:
-            raise RuntimeError(f"Invalid output data dimension: {y.shape[1]} != {self.output_dim}!")
+            raise ValueError(f"Invalid output data dimension: {y.shape[1]} != {self.output_dim}!")
 
 
 class BinaryClsTask(SupervisedTask):
-    def build_graph(self, graph):
+    def build_graph(self, *args, **kwargs):
         pass
 
 
 class MultiClsTask(SupervisedTask):
-    def build_graph(self, graph):
+    def build_graph(self, *args, **kwargs):
         pass
 
 
 class UnsupervisedTask(Task):
-    def validate_data(self, data):
-        if not isinstance(data, np.ndarray):
-            raise RuntimeError("Output data should be numpy.array!")
+    def validate_data(self, data: np.ndarray):
         if len(data.shape) != 2:
-            raise RuntimeError("Output data should be rank 2!")
+            raise ValueError("Output data should be rank 2!")
         if data.shape[1] != self.output_dim:
-            raise RuntimeError(
+            raise ValueError(
                 f"Invalid output data dimension: {data.shape[1]} != {self.output_dim}!")
 
 
 class AutoEncoderTask(UnsupervisedTask):
-    def build_graph(self, graph):
+    def build_graph(self, *args, **kwargs):
         pass
