7fba6b3e1ac9caf1df8d7506b04ccf8d6f666757
noobOriented <jsaon92@gmail.com>
2018-10-09 18:00:00 +0800

    add several typing, change Error type.

--- encoder/encoder.py
+++ encoder/encoder.py
@@ -1,34 +1,39 @@
+import numpy as np
 import tensorflow as tf
 
 _SCOPE_NAME = "Encoder"
 
 
 class Encoder:
-    def __init__(self, input_dim: int):
+    def __init__(self, input_dim: int, output_dim: int):
         self.input_dim = input_dim
+        self.output_dim = output_dim
+        self._set_up()
+
+    def _set_up(self):
         self.graph = tf.Graph()
         with self.graph.as_default(), tf.variable_scope(_SCOPE_NAME):
-            self.build_graph()
+            self._build_graph()
         self.sess = tf.Session(graph=self.graph)
         self.saver = tf.train.Saver(
             var_list=self.graph.get_collection(
                 tf.GraphKeys.GLOBAL_VARIABLES, scope=_SCOPE_NAME),
         )
 
-    def build_graph(self):
+    def _build_graph(self):
         # TODO
         pass
 
-    def encode(self, X):
+    def encode(self, X: np.ndarray) -> np.ndarray:
+        self.validate_data(X)
         # TODO
-        pass
 
     def validate_data(self, data):
         x = data[0] if isinstance(data, tuple) else data
         if len(x.shape) != 2:
-            raise RuntimeError("Input data should be rank 2!")
+            raise ValueError("Input data should be rank 2!")
         if x.shape[1] != self.input_dim:
-            raise RuntimeError(f"Invalid input data dimension: {x.shape[1]} != {self.input_dim}!")
+            raise ValueError(f"Invalid input data dimension: {x.shape[1]} != {self.input_dim}!")
 
     @classmethod
     def load(self, path):
